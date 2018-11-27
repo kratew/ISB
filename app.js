c596a0ae63e3ec3570eb4483afe0f050c09a4d46
kratew <kratew@gmail.com>
2018-11-27 20:55:15 +0900

    수정.

--- app.js
+++ app.js
@@ -6,6 +6,19 @@
 
 
 var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // 템플릿 엔진 ejs 사용 선언. res.render 메소드에서 .ejs 생략 가능
+app.set('views', './views'); // views 파일들이 있는 경로 설정
+
+
 
 console.log("Hello World!");

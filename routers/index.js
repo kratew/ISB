bf08ebb618986991049fec15c3b12fdfc201bb02
kratew <kratew@gmail.com>
2018-11-28 10:33:02 +0900

    수정.

--- routers/index.js
+++ routers/index.js
@@ -12,9 +12,8 @@ module.exports = function(){
     require('express-group-routes');   // 그룹 라우팅
 
     router.get('/', function(req, res){
-        res.render('./main', {title: "홈"}, function(){
-            console.log("Main Page Rendered.");
-        });
+        res.render('./main', {title: "홈"});
+        console.log("Main Page Rendered.");
     });
 
     return router;

7b42ae118c14d16b4244d5c9289ebd8a1756fb07
kratew <kratew@gmail.com>
2018-11-28 12:15:06 +0900

    reset.

---
+++ routers/index.js
@@ -0,0 +1,20 @@
+/*
+ *
+ *   Main Index Router
+ *
+ */
+
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(){
+
+    require('express-group-routes');   // 그룹 라우팅
+
+    router.get('/', function(req, res){
+        res.render('./main', {title: "홈"});
+        console.log("Main Page Rendered.");
+    });
+
+    return router;
+};
